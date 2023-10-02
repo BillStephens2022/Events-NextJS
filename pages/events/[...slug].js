8bc7154848b4c019ef8ad693e21935bacea2d4f2
BillStephens2022 <two4onebill@yahoo.com>
2023-10-02 16:02:59 -0400

    added some error handling for events not found

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,10 @@
+import { Fragment } from "react";
 import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/UI/button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEventsPage() {
   const router = useRouter();
@@ -20,7 +24,7 @@ function FilteredEventsPage() {
 
   console.log("numYear: ", numYear);
   console.log("numMonth: ", numMonth);
-  console.log("isNaN?: ", (isNaN(numYear)));
+  console.log("isNaN?: ", isNaN(numYear));
 
   if (
     isNaN(numYear) ||
@@ -30,7 +34,16 @@ function FilteredEventsPage() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return <p>Invalid filter, Please adjust your values.</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter, Please adjust your values.</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   const filteredEvents = getFilteredEvents({
@@ -39,13 +52,25 @@ function FilteredEventsPage() {
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No events found for the chosen filter!</p>
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
+  const date = new Date(numYear, numMonth - 1);
+
   return (
-    <div>
+    <Fragment>
+      <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
-    </div>
+    </Fragment>
   );
 }
 
