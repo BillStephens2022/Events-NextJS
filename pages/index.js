44bdafabeb6da884dd8eb09d128ce8a183a4af95
BillStephens2022 <two4onebill@yahoo.com>
2023-10-08 13:42:37 -0400

    updated HomePage component to include Newsletter Registration component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+import NewsletterRegistration from "../components/input/newsletter-registration";
 
 function HomePage(props) {
   return (
@@ -12,6 +13,7 @@ function HomePage(props) {
           content="Find a lot of great events that allow you to evolve..."
         ></meta>
       </Head>
+      <NewsletterRegistration />
       <h1 className="center home-h1">Event Tracker</h1>
       <h2 className="center home-h2">Featured Events</h2>
       <EventList items={props.events} />
