f6ab8e7eeba9103be75243f9c055e1df93dfcef7
BillStephens2022 <two4onebill@yahoo.com>
2023-10-10 09:44:51 -0400

    set up notification context (react context)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3aef1d8246369fe2"; }
+/******/ 		__webpack_require__.h = function() { return "818ff2865ff57ed7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
